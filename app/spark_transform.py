0985e6e1a70aac2f7cf9aece1b685eaee43c3dad
Chung-god <qkrguscjd254@gmail.com>
2025-01-07 22:48:28 +0900

    update gitignore and transformation

--- app/spark_transform.py
+++ app/spark_transform.py
@@ -1,8 +1,14 @@
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, when
 
 def transform_data(df):
-  # 예제: null 값을 0으로 대체
+    #null 값을 0으로 대체
     df = df.fillna({"PRODUCT_LENGTH": 0, "PRODUCT_TYPE_ID": -1})
-    df.printSchema()
-    print("Data transformed successfully!")
+    
+
+    #새로운 컬럼 추가
+    df = df.withColumn("IS_LNOG_PRODUCT", when(col("PRODUCT_LENGTH")>1000,1).otherwise(0))
+    
+    df = df.filter(col("PRODUCT_TYPE_ID") < 10000)
+    
+    print("Transformation completed successfully!")
     return df
